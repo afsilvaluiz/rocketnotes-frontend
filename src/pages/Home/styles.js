10cbada7a0911290a96e097b72c70f69289a00eb
luiz <afsilvaluiz@gmail.com>
2023-12-24 22:24:53 -0300

    Implementing the active menu.

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -13,32 +13,51 @@ export const Container = styled.div`
     "menu content"
     "newnote content";
 
-  background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
 `
 
 export const Brand = styled.div`
-  grid-area: brand;
-  background: red;
+	grid-area: brand;
 
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	border-bottom-width: 1px;
+	border-bottom-style: solid;
+	border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+	background-color: ${({ theme }) => theme.COLORS.BACKGROUND_1000};
+
+	> h1 {
+		font-size: 24px;
+		color: ${({ theme }) => theme.COLORS.WINE};
+	}
+`;
 
 export const Menu = styled.ul`
 	grid-area: menu;
-	background: green;
+
+	background-color: ${({ theme }) => theme.COLORS.BACKGROUND_1000};
+
+  padding-top:64px;
+  text-align: center;
+
+
+  > li {
+    margin-bottom: 24px;
+  }
 `;
 
 export const Search = styled.div`
 	grid-area: search;
-	background: violet;
 `;
 
 export const Content = styled.div`
 	grid-area: content;
-	background: blue;
 `;
 
 export const NewNote = styled.button`
 	grid-area: newnote;
-	background: yellow;
 `;
