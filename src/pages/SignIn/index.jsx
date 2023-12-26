f50c76ca4ea1c6b1ea909d99e0e44541b79ce54f
luiz <afsilvaluiz@gmail.com>
2023-12-26 08:47:41 -0300

    Implemented the Create Account Page.

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,40 @@
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container, Form } from './styles'
+
+export function SignIn() {
+  return (
+		<Container>
+			<Form>
+				<h1>Rocket Notes</h1>
+				<p>App to save and manage your useful links</p>
+
+        <h2>Log in</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}        
+        />
+
+        <Input
+          placeholder="Password"
+          type="password"
+          icon={FiLock}        
+        />
+
+        <Button title="Enter" />
+
+        <a href="#">
+          Sign Up
+        </a>
+          
+        
+
+			</Form>
+		</Container>
+	);
+}
