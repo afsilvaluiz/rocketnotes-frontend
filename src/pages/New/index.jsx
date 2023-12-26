a8146d660996c0e5df6dd644428c9d280eb466bd
luiz <afsilvaluiz@gmail.com>
2023-12-26 18:56:02 -0300

    Implemented page Create Note.

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,30 +1,45 @@
+import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 
 import { Container, Form } from './styles'
-
-import { Input } from '../../components/Input'
-import { Textarea } from '../../components/Textarea'
-
  
 export function New() {
-  return(
-    <Container>
-      <Header />
+  return (
+		<Container>
+			<Header />
 
-      <main>
-        <Form>
-          <header>
-            <h1>Create note</h1>
-            <a href="/">back</a>
-          </header>
+			<main>
+				<Form>
+					<header>
+						<h1>Create note</h1>
+						<a href="/">back</a>
+					</header>
 
-          <Input placeholder="Title"/>
-          <Textarea placeholder="Comments"/>
+					<Input placeholder="Title" />
+					<Textarea placeholder="Comments" />
+
+					<Section title="Useful links">
+						<NoteItem value="https://google.com" />
+						<NoteItem isNew placeholder="New link" />
+					</Section>
+
+					<Section title="Bookmakers">
+						
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="New tag" />
+            </div>
 
 
+					</Section>
 
-        </Form>
-      </main>
-    </Container>
-  )
+          <Button title="Save" />
+				</Form>
+			</main>
+		</Container>
+	)
 }
