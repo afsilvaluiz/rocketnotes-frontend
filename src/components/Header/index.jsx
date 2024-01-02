d9c63e05894c94e5b2e92e9d24f2041112be47db
luiz <afsilvaluiz@gmail.com>
2024-01-02 12:46:41 -0300

    Method to update the avatar implemented.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,26 @@
 import { RiShutDownLine } from 	'react-icons/ri'
 import { useAuth } from '../../hooks/auth'
 
+import { api } from '../../services/api'
+
 import { Container, Profile, Logout } from './styles'
 
 export function Header() {
-	const { signOut } = useAuth()
+	const { signOut, user } = useAuth()
+
+    const avatarUrl = user.avatar
+      ? `${api.defaults.baseURL}/files/${user.avatar}`
+      : avatarPlaceholder
 
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/afsilvaluiz.png" alt="User image" />
+        <img src={avatarUrl} alt="{user.name}" />
 
         <div>
           <span>Welcome</span>
-          <strong>Luiz Silva</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
