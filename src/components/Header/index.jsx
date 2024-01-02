c521aa6328e64c33e3ec8d243d5e2ae1efa75cea
luiz <afsilvaluiz@gmail.com>
2024-01-02 09:55:06 -0300

    Logout function implemented.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,27 @@
 import { RiShutDownLine } from 	'react-icons/ri'
+import { useAuth } from '../../hooks/auth'
 
 import { Container, Profile, Logout } from './styles'
 
 export function Header() {
+	const { signOut } = useAuth()
+
+
   return (
-		<Container>      
-			<Profile to="/profile">        
-				<img src="https://github.com/afsilvaluiz.png" 
-        alt="User image" />
+    <Container>
+      <Profile to="/profile">
+        <img src="https://github.com/afsilvaluiz.png" alt="User image" />
 
-				<div>
-					<span>Welcome</span>
-					<strong>Luiz Silva</strong>
-				</div>        
-			</Profile>
+        <div>
+          <span>Welcome</span>
+          <strong>Luiz Silva</strong>
+        </div>
+      </Profile>
 
-			<Logout>
-				<RiShutDownLine />
-			</Logout>
-
-		</Container>
-	  )
+      <Logout onClick={signOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
   }
 
