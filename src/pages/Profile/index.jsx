397396c27c9b10de57a2e92a7a3ca364d6ebdfce
luiz <afsilvaluiz@gmail.com>
2024-01-02 10:42:14 -0300

    Method to load user data implemented.

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,42 +1,72 @@
+import { useState } from 'react'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Link } from 'react-router-dom'
 
+import { useAuth } from '../../hooks/auth'
+ 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
+	const { user } = useAuth()
+
+	const [name, setName] = useState(user.name)
+	const [email, setEmail] = useState(user.email)
+	const [passwordOld, setPasswordOld] = useState()
+	const [passwordNew, setPasswordNew] = useState()
+
 	return (
-		<Container>
-			<header>
-				<Link to="/">
-					<FiArrowLeft />
-				</Link>
-			</header>
+    <Container>
+      <header>
+        <Link to="/">
+          <FiArrowLeft />
+        </Link>
+      </header>
 
-			<Form>
-				<Avatar>
-					<img src="https://github.com/afsilvaluiz.png" alt="User image" />
+      <Form>
+        <Avatar>
+          <img src="https://github.com/afsilvaluiz.png" alt="User image" />
 
-					<label htmlFor="avatar">
-						<FiCamera />
-						<input
-						  id="avatar" type="file"
-            />
-					</label>
-				</Avatar>
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
 
-				<Input placeholder="Name" type="text" icon={FiUser} />
+        <Input
+          placeholder="Name"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
 
-				<Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
 
-				<Input placeholder="Current password" type="password" icon={FiLock} />
+        <Input
+          placeholder="Current password"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
+        />
 
-				<Input placeholder="New password" type="password" icon={FiLock} />
+        <Input
+          placeholder="New password"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
+        />
 
-				<Button title="Save" />
-			</Form>
-		</Container>
-	);
+        <Button title="Save" />
+      </Form>
+    </Container>
+  )
 }
