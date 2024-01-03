a667fce52cccb5c9c2fccb26ffe03ef996d492c6
luiz <afsilvaluiz@gmail.com>
2024-01-03 13:13:56 -0300

    Navigation adjustments made.

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 
 import { useNavigate } from 'react-router-dom'
 
@@ -7,6 +6,7 @@ import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
 import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 
@@ -26,6 +26,10 @@ export function New() {
 	const [newTag, setNewTag] = useState("")
 
   const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
 
 	function handleAddLink() {
 		setLinks(prevState => [...prevState, newLink])
@@ -66,7 +70,7 @@ export function New() {
     })
 
     alert('Note created successfully.')
-    navigate("/")
+    navigate(-1)
   }
 
   return (
@@ -77,7 +81,10 @@ export function New() {
         <Form>
           <header>
             <h1>Create note</h1>
-            <Link to="/">back</Link>
+            <ButtonText 
+              title="back"
+              onClick={handleBack}
+            />
           </header>
 
           <Input 
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { useAuth } from '../../hooks/auth'
 
@@ -20,11 +20,16 @@ export function Profile() {
 	const [passwordOld, setPasswordOld] = useState()
 	const [passwordNew, setPasswordNew] = useState()
 
-
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
   const [avatar, setAvatar] = useState(avatarUrl)
   const [avatarFile, setAvatarFile] = useState(null)
+
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
 
 
   async function handleUpdate() {
@@ -50,9 +55,9 @@ export function Profile() {
 	return (
     <Container>
       <header>
-        <Link to="/">
-          <FiArrowLeft />
-        </Link>
+        <button type="button" onClick={handleBack}>
+          <FiArrowLeft size={24}/>
+        </button>
       </header>
 
       <Form>
