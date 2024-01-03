fbb60f4ec966879477fd42df9a1f4a45d8aa8883
luiz <afsilvaluiz@gmail.com>
2024-01-03 12:01:44 -0300

    Implemented the 'view note' function.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { FiPlus, FiSearch } from "react-icons/fi";
 
 import { api } from "../../services/api";
@@ -17,6 +19,8 @@ export function Home() {
 	const [tagsSelected, setTagsSelected] = useState([])
   const [notes, setNotes] = useState([])
 
+  const navigate = useNavigate()
+
   function handleTagSelected(tagName) {
     if(tagName === "all") {
       return setTagsSelected([])
@@ -31,6 +35,11 @@ export function Home() {
     } else {
       setTagsSelected(prevState => [...prevState, tagName])
     }
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`)
+
   }
 	
 	useEffect(() => {
@@ -96,6 +105,7 @@ export function Home() {
               <Note
                 key={String(note.id)}
                 data={note}
+                onClick={() => handleDetails(note.id)}
               />
             ))
           }
